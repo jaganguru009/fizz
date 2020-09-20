3e60cbf076772ece4eeb518d68b14c01f1f7d485
jaganguru009 <jaganguru009@gmail.com>
2020-09-20 15:55:35 +0530

    Fizz buss using node

---
+++ readme.md
@@ -0,0 +1,11 @@
+run npm install
+
+API reference
+
+EndPoint
+localhost:3000/api/fizz
+
+request body
+{
+count:100
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+const validate = require("./middlewares/requestValidations").validate;
+const routes = require("./routes/routes");
+const PORT = 3000;
+const app = express();
+try {
+  app.use(bodyParser.json({ urlencoded: true }));
+
+  //app.all("*", validate);
+
+  app.use("/api", validate, require("./routes/routes"));
+} catch (e) {
+  console.log("Eror occucered " + err);
+}
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Error while running the server on Port  ${PORT}`);
+  } else console.log(`Server started on   ${PORT}`);
+});
